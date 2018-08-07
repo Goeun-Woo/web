3e81fff45eee0c217dbe45e989a8d13fca3d2492
Goeun-Woo <8272131a@gmail.com>
2018-08-07 16:19:42 +0900

    Updates

--- navbar-anim.js
+++ navbar-anim.js
@@ -1,7 +1,3 @@
-//네비게이션 바 제이쿼리 애니메이션 적용
-//-------------------------------------------------------------------------------------
-// ** Function 1 . Navigation Bar Click Event **
-//-------------------------------------------------------------------------------------
 //Function 1-1 . Smooth Scrolling On Click
 $(document).ready(function() {
   $("a[href*=\\#]").click(function(event) {
@@ -9,16 +5,14 @@ $(document).ready(function() {
     event.preventDefault();
     $("html, body").animate({ scrollTop: $(linkhref).offset().top }, 1000);
   });
-  //-------------------------------------------------------------------------------------
+
   //Function 1-2 . Navigation Bar got Activated On Click
   $("nav ul a").on("click", function(e) {
     $("nav ul a").removeClass("active");
     $(this).addClass("active");
   });
 });
-//-------------------------------------------------------------------------------------
-// ** Function 2 . Navigation Bar Scroll Event **
-//-------------------------------------------------------------------------------------
+
 //Function 2-1 . Navigation Bar Shrink When Scroll Down from Top
 $(window).scroll(function() {
   if ($(document).scrollTop()) {
@@ -26,7 +20,7 @@ $(window).scroll(function() {
   } else {
     $("nav").removeClass("shrink");
   }
-  //-------------------------------------------------------------------------------------
+
   //Function 2-2 . Scroll spy
   $(".NavTarget").each(function() {
     if ($(window).scrollTop() + 50 >= $(this).offset().top) {
@@ -37,5 +31,4 @@ $(window).scroll(function() {
       $("nav ul a[href=#" + id + "]").addClass("active");
     }
   });
-  //-------------------------------------------------------------------------------------
 });
