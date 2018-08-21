b9db9b8a609c5673ede8746f3bdc95440a4fb418
Goeun-Woo <8272131a@gmail.com>
2018-08-21 22:57:04 +0900

    hamburger button bug fixed

--- public/component-anim.js
+++ public/component-anim.js
@@ -1,5 +1,4 @@
-// 컴포넌트 제이쿼리 애니메이션 적용 - 스크롤 트리거
-
+// Function 4. Scroll Trigger :  WEB FRONT-END 타이틀 애니메이션, 위에서 아래로 내려오기
 $(window).scroll(function() {
   $(".NavTarget").each(function() {
     if ($(window).scrollTop() >= $(this).offset().top - 300) {
--- public/navbar-anim.js
+++ public/navbar-anim.js
@@ -1,57 +1,57 @@
-//Function 1 . Smooth Scrolling On Click
 $(document).ready(function() {
+  //Function 1-1 . Smooth Scrolling On Click
   $("a[href*=\\#]").click(function(event) {
-      var linkhref = $(this).attr("href");
-      event.preventDefault();
-      $("html, body").animate({
-          scrollTop: $(linkhref).offset().top
-      }, 1000);
+    var linkhref = $(this).attr("href");
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $(linkhref).offset().top }, 1000);
   });
 
-  //Function 2 . Navigation Bar got Activated On Click
+  //Function 1-2. Navigation Bar got Activated On Click
   $("nav ul a").on("click", function(e) {
-      $("nav ul a").removeClass("active");
-      $(this).addClass("active");
+    $("nav ul a").removeClass("active");
+    $(this).addClass("active");
   });
 
-  //Function 3. mobile nav menu disappear on click
+  //Function 1-3. Navigation Menu disappear when menu clicked.
   $("input").click(function() {
-      var mobileNav = $("input[id='toggle']").is(":checked");
-      if (mobileNav) {
-          $('.mobile ul').click(function() {
-              $('#toggle').attr('checked', false);
-          });
-      }
-  })
-
-
-  //Function 4 . Navigation Bar Shrink When Scroll Down from Top
-  $(window).scroll(function() {
-      if ($(document).scrollTop()) {
-          $("nav").addClass("shrink");
-      } else {
-          $("nav").removeClass("shrink");
-      }
-
-      //Function 5. Scroll spy
-      $(".NavTarget").each(function() {
-          if ($(window).scrollTop()>= $(this).offset().top) {
-              var id = $(this).attr("id");
-              $("nav ul")
-                  .find("a")
-                  .removeClass("active");
-              $("nav ul a[href=#" + id + "]").addClass("active");
-          }
+    var mobileNav = $("input[id='toggle']").is(":checked");
+    console.log(mobileNav);
+    if (mobileNav) {
+      $(".mobile ul").click(function() {
+        $("#toggle").attr("checked", false);
       });
+    }
   });
-
-  //Function 6. classname 'mobile' added
-  $(window).resize(function() {
-      var width = window.innerWidth;
-      if (width <= 500) {
-          $("nav").addClass("mobile");
-      } else {
-          $("nav").removeClass("mobile");
-      }
-  }).resize();
 });
+
+//Function 2-1 . Navigation Bar Shrink When Scroll Down from Top
+$(window).scroll(function() {
+  if ($(document).scrollTop()) {
+    $("nav").addClass("shrink");
+  } else {
+    $("nav").removeClass("shrink");
+  }
+
+  //Function 2-2 . scroll spy
+  $(".NavTarget").each(function() {
+    if ($(window).scrollTop() >= $(this).offset().top - 50) {
+      var id = $(this).attr("id");
+      $("nav ul")
+        .find("a")
+        .removeClass("active");
+      $("nav ul a[href= #" + id + "]").addClass("active");
+    }
+  });
+});
+
+//Function 3. Mobile Hamburger Button
+$(window)
+  .resize(function() {
+    var width = window.innerWidth;
+    if (width <= 500) {
+      $("nav").addClass("mobile");
+    } else {
+      $("nav").removeClass("mobile");
+    }
+  })
+  .resize();
